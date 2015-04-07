df40e487a8834bfadbd3cab24b5f93f57adfea5e
omar <omarshariffathi@hotmail.com>
2015-04-08 00:55:10 +0200

    Updated security with login views

--- my_project/views.py
+++ my_project/views.py
@@ -9,80 +9,59 @@ from my_project import forms
 
 
 def login(request):
-    if request.user.is_anonymous():
-        return render(request, "500.html", {})
-    else:
-        print request.POST
-        c = {}
-        c.update(csrf(request))
-        return render_to_response('login.html', c)
+    print request.POST
+    c = {}
+    c.update(csrf(request))
+    return render_to_response('login.html', c)
 
 
 def logout(request):
-    if request.user.is_anonymous():
-        return render(request, "500.html", {})
-    else:
-        auth.logout(request)
-        return render_to_response('logout.html')
+    auth.logout(request)
+    return render_to_response('logout.html')
 
 
 def logged_in(request):
-    if request.user.is_anonymous():
-        return render(request, "500.html", {})
-    else:
-        print request.POST
-        return render_to_response(
-            'logged_in.html',
-            {'first': request.user.first_name,
-             'last': request.user.last_name},
-        )
+    print request.POST
+    return render_to_response(
+        'logged_in.html',
+        {'first': request.user.first_name,
+         'last': request.user.last_name},
+    )
 
 
 def auth_view(request):
-    if request.user.is_anonymous():
-        return render(request, "500.html", {})
-    else:
-        print request.POST
-        username = request.POST.get('username', '')
-        password = request.POST.get('password', '')
-        user = auth.authenticate(username=username, password=password)
+    print request.POST
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
 
-        if user is not None:
-            auth.login(request, user)
-            return HttpResponseRedirect('/g/')
-        else:
-            return HttpResponseRedirect('/accounts/invalid')
+    if user is not None:
+        auth.login(request, user)
+        return HttpResponseRedirect('/g/')
+    else:
+        return HttpResponseRedirect('/accounts/invalid')
 
 
 def invalid_login(request):
-    if request.user.is_anonymous():
-        return render(request, "500.html", {})
-    else:
-        return render_to_response('invalid_login.html')
+    return render_to_response('invalid_login.html')
 
 
 def register_user(request):
-    if request.user.is_anonymous():
-        return render(request, "500.html", {})
-    else:
-        if request.method == 'POST':
-            print request.POST
-            form = forms.UserCreateForm(request.POST)
-            context = {"form": form}
-            template = "register.html"
-            if form.is_valid():
-                form.save()
-                return HttpResponseRedirect('/accounts/register_success')
+    if request.method == 'POST':
+        print request.POST
+        form = forms.UserCreateForm(request.POST)
+        context = {"form": form}
+        template = "register.html"
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/accounts/register_success')
 
-        args = {}
-        args.update(csrf(request))
+    args = {}
+    args.update(csrf(request))
 
-        args['form'] = forms.UserCreateForm()
-        return render_to_response('register.html', args)
+    args['form'] = forms.UserCreateForm()
+    return render_to_response('register.html', args)
 
 
 def register_success(request):
-    if request.user.is_anonymous():
-        return render(request, "500.html", {})
-    else:
-        return render_to_response('register_success.html')
+    return render_to_response('register_success.html')
